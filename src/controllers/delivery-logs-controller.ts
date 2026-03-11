07cb4ac95e17ed9f6ccd9695e8ff28e31a35fb4a
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-11 18:58:34 -0300

    feat(delivery-logs): implement log creation with early returns and business rules

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,11 +1,43 @@
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
-    // Isso é um "Stub" ou "Placeholder". 
-    // Uma resposta fake só pra gente testar no Insomnia se as portas, rotas e catracas (middlewares)
-    // estão funcionando antes de perdermos tempo programando a lógica de banco de dados real.
-    return response.json({ message: "ok!" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    // [!] Early Return / Fail-Fast: Matamos a requisição no topo com erros se algo não cheirar bem.
+    // 1. A caixa solicitada sequer existe no banco?
+    if (!delivery) {
+      throw new AppError("delivery not found", 404);
+    }
+
+    // 2. Business Rule (Regra de Negócio): A empresa proíbe gerar histórico (log) para uma
+    // caixa que AINDA está em processamento base. (Exigência de Logística).
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped", 404);
+    }
+
+    // Tendo passado pelos 'Seguranças' (Early Returns), o código flui livremente para a inserção.
+    await prisma.deliveryLog.create({
+      data: {
+        // FK: Amarramos este caderninho (Log) à verdadeira caixa (Delivery).
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
