d3f4f7ae3970d21b0d89efdf20d30971a59e419a
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-14 15:21:11 -0300

    test(users): add targeted afterAll cleanup using created user id

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,14 @@
 import request from "supertest";
+import { prisma } from "@/database/prisma";
 import { app } from "@/app";
 
 describe("UsersController", () => {
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
+
   it("should create a new user succesfully", async () => {
     const response = await request(app).post("/users").send({
       name: "Test User",
@@ -12,5 +19,7 @@ describe("UsersController", () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
     expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id;
   });
 });
